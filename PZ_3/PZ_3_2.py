4de430bb46876b3d56284e9d4f5953544debb9c4
IvanDanchenko <th3b3l1v3r@gmail.com>
2024-11-10 18:13:24 +0300

    Update PZ_3_2.py

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -6,32 +6,44 @@
 над числами указанное действие и вывести результат.
 '''
 
-N = int(input("Введите число от 1 до 4 (1 = сложение, 2 = вычитание, 3 = умножение, 4 = деление): "))
-if N < 1:
-    print("Число должно быть от 1 до 4.")
-    exit()
-elif N > 4:
-    print("Число должно быть от 1 до 4.")
-    exit()
+def sum(A, B):
+    return A + B
+
+def sub(A, B):
+    return A - B
+
+def mltp(A, B):
+    return A * B
+
+def div(A, B):
+    return A / B
+
+while True: #Проверка действия на целое число от 1 до 4
+    N = input("Введите число от 1 до 4 (1 = сложение, 2 = вычитание, 3 = умножение, 4 = деление): ")
+    try:
+        operation = int(N)
+        if operation < 1 or operation > 4:
+            print("Число должно быть от 1 до 4.")
+            continue
+        break
+    except ValueError:
+        print("Ошибка! Повторите попытку.")
+
 
 A = float(input("Введите первое вещественное число A: "))
 B = float(input("Введите второе вещественное число B: "))
-if B == 0:
-    print("Число B не должно быть равным нулю.")
-    exit()
 
-if N == 1:
-    result = A+B
+if operation == 1:
+    result = sum(A,B)
     print("Результат сложения: ", result)
-elif N == 2:
-    result = A-B
+elif operation == 2:
+    result = sub(A,B)
     print("Результат вычитания: ", result)
-elif N == 3:
-    result = A*B
+elif operation == 3:
+    result = mltp(A,B)
     print("Результат умножения: ", result)
-elif N == 4:
-    result = A/B
+elif operation == 4:
+    result = div(A,B)
     print("Результат деления: ", result)
-
 
 
